251743c5e3fe5db93c98dfbb23bf1956d2e7bc48
= <kaweesha.mr@gmail.com>
2023-11-24 12:42:17 +0530

    url routing learned

--- server.js
+++ server.js
@@ -1,9 +1,35 @@
 const http = require('http');
+const fs = require('fs');
 
 
 const server = http.createServer((req,res) => {
 
-    console.log('request made');
+    res.setHeader('Content-Type','text/html');
+    let path = './views/';
+
+    switch(req.url){
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            path += '404.html';
+            break;
+    }
+
+    //read the html file
+
+    fs.readFile(path,(err,data) => {
+        if(err){
+            console.log(err);
+            res.end();
+        }
+        else{
+            res.end(data);
+        }
+    })
 });
 
 
