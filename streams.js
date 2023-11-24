56dc9c47acfae68fa2c075a7f3cb1443db28d79a
= <kaweesha.mr@gmail.com>
2023-11-24 10:58:45 +0530

    learning server side

--- streams.js
+++ streams.js
@@ -1,5 +1,21 @@
 //there are 2 1 is read streams and write streams
-fstat.js
-// Path: fstat.js
-//fstat is used to get the information about the file
-// const fs = 
+
+
+
+
+const fs = require('fs');
+
+
+const readStream = fs.createReadStream('/docs/blog3.txt', {encoding: 'utf8'});
+
+readStream.on('data',(chunk) => {
+
+
+    comsole.log('----NEW CHUNK----');
+    console.log(chunk.toString());
+
+})
+
+
+
+
