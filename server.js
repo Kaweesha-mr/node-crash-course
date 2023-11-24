f37a4e968aa3f356687a7fc3e72e91ab4e9d292b
= <kaweesha.mr@gmail.com>
2023-11-24 20:35:45 +0530

    learning npm

--- server.js
+++ server.js
@@ -1,39 +1,57 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash')
 
 
-const server = http.createServer((req,res) => {
+const server = http.createServer((req, res) => {
 
-    res.setHeader('Content-Type','text/html');
+    //loadash
+
+    const num = _.random(0,20);
+    const greet = _.once( () => {
+        
+        console.log("hellow")
+
+    } ) 
+
+    console.log(num);
+    res.setHeader('Content-Type', 'text/html');
     let path = './views/';
 
-    switch(req.url){
+    switch (req.url) {
         case '/':
             path += 'index.html';
+            req.statusCode = 200;
             break;
         case '/about':
             path += 'about.html';
+            req.statusCode = 200;
+            break;
+        case '/about':
+            req.setHeader('Location', '/about');
+            req.statusCode = 301;
             break;
         default:
             path += '404.html';
+            req.statusCode = 404;
             break;
     }
 
     //read the html file
 
-    fs.readFile(path,(err,data) => {
-        if(err){
+    fs.readFile(path, (err, data) => {
+        if (err) {
             console.log(err);
             res.end();
         }
-        else{
+        else {
             res.end(data);
         }
     })
 });
 
 
-server.listen(3000,'localhost', () => {
+server.listen(3000, 'localhost', () => {
     console.log('listing for request on port 3000')
 })
 
