fe40bde9fbfa6bfa0ed688a9e007661935d949a0
= <kaweesha.mr@gmail.com>
2023-11-26 11:55:09 +0530

    file system updated

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 const express = require('express');
 
-
-//express app
-
+// express app
 const app = express();
-app.set('view engine', 'ejs');
 
-//listen for request
-
+// listen for requests
 app.listen(3000);
 
-app.get('/', (req,res) => {
-    //res.send('<p>Home page </P>');
-    res.render('index', {title: "Home"});
-})
+// register view engine
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews');
 
-app.get('/ABOUT', (req,res) => {
-    res.render('about',{title: "About Us"});
+app.get('/', (req, res) => {
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', { title: 'Home', blogs });
+});
 
-})
-app.get('/blogs/create', (req,res)=>{
-    res.render('create',{title: "New Blog"});
-})
-//404 pages
-app.use((req,res)=>{
-    res.render('404',{title: "File Not found"});
-})
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
 
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
+});
 
-
-
-
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
